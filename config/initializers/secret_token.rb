393faa7e78f3267475507fa3002dc9f54a97d961
Ajit Shenoy <ajit@spirolution.com>
2011-09-05 15:59:59 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ef998b0d971ac9e70322d5b03fba26a61b80be754fb69826cea18333987cef9b4dde428194e268f6e406fda52e04b8a3a1834f39241e6b97c6cc28ea6aea1b65'
